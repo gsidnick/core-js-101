1d0225e31fb8746f8d4e9fcff1b88701d95534a6
Nick GS <gsidnick@gmail.com>
2023-05-29 20:54:29 +0300

    feat: add solutions to 05-objects-tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,10 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => width * height;
 }
 
 
@@ -35,8 +37,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +53,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const object = JSON.parse(json);
+  Object.setPrototypeOf(object, proto);
+  return object;
 }
 
 
@@ -111,32 +115,112 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  selector: '',
+  isExistElement: false,
+  isExistId: false,
+  isExistClass: false,
+  isExistAttr: false,
+  isExistPseudoClass: false,
+  isExistPseudoElement: false,
+
+  element(value) {
+    const self = { ...this };
+    const {
+      isExistElement,
+      isExistId,
+      isExistClass,
+      isExistAttr,
+      isExistPseudoClass,
+      isExistPseudoElement,
+    } = this;
+    if (isExistId
+      || isExistClass
+      || isExistAttr
+      || isExistPseudoClass
+      || isExistPseudoElement
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    if (isExistElement) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    self.selector = `${this.selector}${value}`;
+    self.isExistElement = true;
+    return self;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    const self = { ...this };
+    const {
+      isExistId,
+      isExistClass,
+      isExistAttr,
+      isExistPseudoClass,
+      isExistPseudoElement,
+    } = this;
+    if (isExistClass
+      || isExistAttr
+      || isExistPseudoClass
+      || isExistPseudoElement
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    if (isExistId) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    self.selector = `${this.selector}#${value}`;
+    self.isExistId = true;
+    return self;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    const self = { ...this };
+    const {
+      isExistAttr,
+      isExistPseudoClass,
+      isExistPseudoElement,
+    } = this;
+    if (isExistAttr
+      || isExistPseudoClass
+      || isExistPseudoElement
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    self.selector = `${this.selector}.${value}`;
+    self.isExistClass = true;
+    return self;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    const self = { ...this };
+    const {
+      isExistPseudoClass,
+      isExistPseudoElement,
+    } = this;
+    if (isExistPseudoClass
+      || isExistPseudoElement
+    ) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    self.selector = `${this.selector}[${value}]`;
+    self.isExistAttr = true;
+    return self;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    const self = { ...this };
+    const { isExistPseudoElement } = this;
+    if (isExistPseudoElement) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    self.selector = `${this.selector}:${value}`;
+    self.isExistPseudoClass = true;
+    return self;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    const self = { ...this };
+    const { isExistPseudoElement } = this;
+    if (isExistPseudoElement) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    self.selector = `${this.selector}::${value}`;
+    self.isExistPseudoElement = true;
+    return self;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const self = { ...this };
+    self.selector = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return self;
+  },
+
+  stringify() {
+    return this.selector;
   },
 };
 
